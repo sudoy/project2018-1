249c9b43f98959048470d03912e193e304f56420
tsuchitoi.keita <tsuchitoi.keita@tsd-201601-0004>
2018-06-08 16:34:36 +0900

    検索ページを作成（エラー文でない）

---
+++ WebContent/WEB-INF/S0020.jsp
@@ -0,0 +1,87 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<jsp:include page="_header.jsp" />
+		<title>売上検索条件入力|物品売上管理システム</title>
+	</head>
+	<body>
+
+		<jsp:include page="_navbar.jsp" />
+
+		<div class="container">
+
+
+			<div class="row">
+				<h1>売上検索条件入力</h1>
+			</div>
+
+			<div class="row">
+				<form class="form-horizontal" action="S0020.html" method="post">
+					<div class="form-group">
+						<label for="salesDate" class="col-sm-2 control-label">販売日</label>
+						<div class="col-sm-2">
+							<input type="text" class="form-control" id="salesDate" name="start" placeholder="販売日(検索開始日)" value="">
+						</div>
+
+						<div class="form-control-static col-sm-1 text-center">～</div>
+
+						<div class="col-sm-2">
+							<input type="text" class="form-control" name="end" placeholder="販売日(検索終了日)" value="">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label for="person" class="col-sm-2 control-label">担当</label>
+						<div class="col-sm-5">
+							<select class="form-control" id="person">
+								<option value="" selected>選択してください</option>
+								<c:forEach var="account" items="${accountList}">
+									<option value="${account}">${account}</option>
+								</c:forEach>
+							</select>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label for="category" class="col-sm-2 control-label">商品カテゴリー</label>
+						<div class="col-sm-5">
+							<select class="form-control" id="category">
+								<option value="" selected>選択してください</option>
+								<c:forEach var="category" items="${categoryList}">
+									<option value="${category}">${category}</option>
+								</c:forEach>
+							</select>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label for="name" class="col-sm-2 control-label">商品名 <span class="badge">部分一致</span></label>
+						<div class="col-sm-5">
+							<input type="text" class="form-control" id="name" name="trade_name" placeholder="商品名" value="">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label for="note" class="col-sm-2 control-label">備考 <span class="badge">部分一致</span></label>
+						<div class="col-sm-5">
+							<input type="text" class="form-control" id="note" name="note" placeholder="備考" value="">
+						</div>
+					</div>
+
+					<div class="form-group">
+						<div class="col-sm-offset-3">
+							<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> 検 索</button>
+							<a href="S0020.html" class="btn btn-default"> クリア</a>
+						</div>
+					</div>
+
+				</form>
+			</div>
+
+		</div><!-- /container -->
+	</body>
+</html>
---
+++ src/com/abc/asms/S0020Servlet.java
@@ -0,0 +1,121 @@
+package com.abc.asms;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import utils.ServletUtils;
+
+@WebServlet("/S0020.html")
+public class S0020Servlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+		List<String> categoryList = ServletUtils.categoryList(req);
+		req.setAttribute("categoryList", categoryList);
+
+		List<String> accountList = ServletUtils.accountList(req);
+		req.setAttribute("accountList", accountList);
+		getServletContext().getRequestDispatcher("/WEB-INF/S0020.jsp").forward(req, resp);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		req.setCharacterEncoding("utf-8");
+
+
+		HttpSession session = req.getSession();
+		List<String> errors = validate(req);
+
+		if(errors.size() != 0) {
+			session.setAttribute("errors", errors);
+
+			getServletContext().getRequestDispatcher("/WEB-INF/S0020.jsp")
+			.forward(req, resp);
+			return;
+
+		}
+
+
+		getServletContext().getRequestDispatcher("/WEB-INF/S0021.jsp").forward(req, resp);
+	}
+
+	private List<String> validate(HttpServletRequest req){
+
+		List<String> list = new ArrayList<>();
+		if(!req.getParameter("start").equals("")) {
+			//形式の判定
+			try {
+				DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+				df.setLenient(false);
+			    String s1 = req.getParameter("start");
+			    String s2 = df.format(df.parse(s1));
+
+			    if(s1.equals(s2)) {
+
+			    }else {
+			    	list.add("販売日（検索開始日）を正しく入力してください。");
+			    }
+
+			}catch(ParseException p) {
+
+				list.add("販売日（検索開始日）を正しく入力してください。");
+
+			}
+		}
+		if(!req.getParameter("end").equals("")) {
+			//形式の判定
+			try {
+				DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+				df.setLenient(false);
+			    String s1 = req.getParameter("end");
+			    String s2 = df.format(df.parse(s1));
+
+			    if(s1.equals(s2)) {
+
+			    }else {
+			    	list.add("販売日（検索終了日）を正しく入力してください。");
+			    }
+
+			}catch(ParseException p) {
+
+				list.add("販売日（検索終了日）を正しく入力してください。");
+
+			}
+		}
+
+		if(!req.getParameter("end").equals("") && !req.getParameter("start").equals("")) {
+			SimpleDateFormat fmt = new SimpleDateFormat("yyyy/MM/dd");
+			fmt.setLenient(false);
+			try {
+				Date fs = fmt.parse(req.getParameter("start"));
+				Date fe = fmt.parse(req.getParameter("end"));
+				if(!fs.before(fe)) {
+					list.add("販売日（検索開始日）が販売日（検索終了日）より後の日付になっています。");
+				}
+			} catch (Exception e1) {
+
+			}
+		}
+
+		return list;
+
+
+	}
+
+}
