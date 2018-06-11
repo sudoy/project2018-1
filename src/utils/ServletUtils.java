1bdc83ce63923ea225df54dc482ef6344f62fe15
tsuchitoi.keita <tsuchitoi.keita@tsd-201601-0004>
2018-06-11 11:37:37 +0900

    ServletUtilsにcategory,accountの変換を追加。

--- src/utils/ServletUtils.java
+++ src/utils/ServletUtils.java
@@ -98,4 +98,182 @@ public class ServletUtils {
 
 		return accountList;
 	}
+
+	public static String pairAccount(String account) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = null;
+		ResultSet rs = null;
+
+		String accountId = null;
+
+		if(account.equals("")) {
+			return null;
+		}
+
+		try {
+			con = DBUtils.getConnection();
+
+			sql = "select account_id, name from account where name=? order by account_id";
+
+			ps = con.prepareStatement(sql);
+
+			ps.setString(1, account);
+
+			rs = ps.executeQuery();
+
+			rs.next();
+
+			accountId = rs.getString("account_id");
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				DBUtils.close(con);
+				DBUtils.close(ps);
+				DBUtils.close(rs);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+		return accountId;
+	}
+
+	public static String pairCategory(String category) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = null;
+		ResultSet rs = null;
+
+		String categoryId = null;
+
+		if(category.equals("")) {
+			return null;
+		}
+
+		try {
+			con = DBUtils.getConnection();
+
+			sql = "select category_id, category_name from categories where category_name=? order by category_id";
+
+			ps = con.prepareStatement(sql);
+
+			ps.setString(1, category);
+
+			rs = ps.executeQuery();
+
+			rs.next();
+
+			categoryId = rs.getString("account_id");
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				DBUtils.close(con);
+				DBUtils.close(ps);
+				DBUtils.close(rs);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+		return categoryId;
+	}
+
+	public static String parseAccountName(int accountId) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = null;
+		ResultSet rs = null;
+
+		String name = null;
+
+		if(accountId == 0) {
+			return null;
+		}
+
+		try {
+			con = DBUtils.getConnection();
+
+			sql = "select account_id, name from accounts where account_id=? order by account_id";
+
+			ps = con.prepareStatement(sql);
+
+			ps.setString(1, String.valueOf(accountId));
+
+			rs = ps.executeQuery();
+
+			rs.next();
+
+			name = rs.getString("name");
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				DBUtils.close(con);
+				DBUtils.close(ps);
+				DBUtils.close(rs);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+		return name;
+
+	}
+
+	public static String parseCategoryName(int categoryId) {
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = null;
+		ResultSet rs = null;
+
+		String name = null;
+
+		if(categoryId == 0) {
+			return null;
+		}
+
+		try {
+			con = DBUtils.getConnection();
+
+			sql = "select category_id, category_name from categories where category_id=? order by category_id";
+
+			ps = con.prepareStatement(sql);
+
+			ps.setString(1, String.valueOf(categoryId));
+
+			rs = ps.executeQuery();
+
+			rs.next();
+
+			name = rs.getString("category_name");
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				DBUtils.close(con);
+				DBUtils.close(ps);
+				DBUtils.close(rs);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+		return name;
+
+	}
 }
